67261e924d77d57276785d1733a12188b9f4edb1
katelijacobsen <katjamaehleke98@gmail.com>
2024-09-23 23:08:59 +0200

    example

---
+++ todo-list/myscript.js
@@ -0,0 +1,27 @@
+const todoForm = document.querySelector("#form");
+const todoInput = document.querySelector(".input");
+const toDos = document.querySelector(".todos");
+
+// Prevent form from default
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //Former Data/ tilføjer data i vores input-felt
+    const data = new FormData(todoForm);
+    const text = data.get("todo")
+
+    //tilføjer indhold inden i elementet <li>
+    const textNode = document.createTextNode(text); 
+    //tilføjer nyt element i vores HTML
+    const li = document.createElement("li");
+    li.appendChild(textNode);
+    li.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        console.log(e.button)
+        li.remove();
+
+    })
+    
+    toDos.prepend(li);
+    
+    todoForm.reset();
+});
