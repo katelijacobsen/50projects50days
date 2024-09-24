ab4cf2aa04ce3dd9fb9e75695695ba2c16391ec0
katelijacobsen <katjamaehleke98@gmail.com>
2024-09-24 09:39:23 +0200

    add left-click to li for completing todo

--- todo-list/myscript.js
+++ todo-list/myscript.js
@@ -6,6 +6,7 @@ const toDos = document.querySelector(".todos");
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
     //Former Data/ tilføjer data i vores input-felt
+    //Object
     const data = new FormData(todoForm);
     const text = data.get("todo")
 
@@ -14,13 +15,18 @@ todoForm.addEventListener("submit", (e) => {
     //tilføjer nyt element i vores HTML
     const li = document.createElement("li");
     li.appendChild(textNode);
+    
+    li.addEventListener("click", () => {
+        li.classList.toggle("completed")
+    });
+    
     li.addEventListener("contextmenu", (e) => {
         e.preventDefault();
         console.log(e.button)
         li.remove();
 
-    })
-    
+    });
+    //modsat til appendChild
     toDos.prepend(li);
     
     todoForm.reset();
